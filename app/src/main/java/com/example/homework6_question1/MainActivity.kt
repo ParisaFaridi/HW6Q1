4b54e7dc0e63a95454af22a289174457d3d5e820
Pareeesa <parisa.faridi80@gmail.com>
2022-02-13 14:02:50 +0330

    Correct views' IDs and import Binding

--- app/src/main/java/com/example/homework6_question1/MainActivity.kt
+++ app/src/main/java/com/example/homework6_question1/MainActivity.kt
@@ -2,10 +2,16 @@ package com.example.homework6_question1
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.RadioButton
+import com.example.homework6_question1.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
+
     }
 }
