11b4216e73f93678987d9bd496729a4b6d98fe11
Pareeesa <parisa.faridi80@gmail.com>
2022-02-13 14:06:45 +0330

    Implement share preferences and a view showing info

--- app/src/main/java/com/example/homework6_question1/MainActivity.kt
+++ app/src/main/java/com/example/homework6_question1/MainActivity.kt
@@ -13,5 +13,28 @@ class MainActivity : AppCompatActivity() {
         val view = binding.root
         setContentView(view)
 
+        val sharedPreferences=getSharedPreferences("preferences", MODE_PRIVATE)
+        val editor=sharedPreferences.edit()
+
+        binding.btnRegister.setOnClickListener{
+            var name =binding.etFullName.text.toString()
+            var userName =binding.etUserName.text.toString()
+            var email =binding.etEmail.text.toString()
+            var password =binding.etFullName.text.toString()
+            if (password != binding.etRetypePassword.text.toString())
+                binding.etRetypePassword.error="password and retyped password do not match!"
+            val checkedGenderRadioButton=binding.radioGroup.checkedRadioButtonId
+            var gender =findViewById<RadioButton>(checkedGenderRadioButton).text.toString()
+
+            editor.apply {
+                putString("Full Name",name)
+                putString("User Name",userName)
+                putString("Email",email)
+                putString("PassWord",password)
+                putString("Gender",gender)
+                apply()
+            }
+        }
+
     }
 }
