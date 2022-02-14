f789266d2a54d763b86f903fc00b6c70b57ce4a9
Pareeesa <parisa.faridi80@gmail.com>
2022-02-14 16:19:38 +0330

    Complete the project

--- app/src/main/java/com/example/homework6_question1/MainActivity.kt
+++ app/src/main/java/com/example/homework6_question1/MainActivity.kt
@@ -2,7 +2,9 @@ package com.example.homework6_question1
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 import android.widget.RadioButton
+import android.widget.Toast
 import com.example.homework6_question1.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
@@ -22,31 +24,59 @@ class MainActivity : AppCompatActivity() {
         val editor=sharedPreferences.edit()
 
         binding.btnRegister.setOnClickListener{
-            name =binding.etFullName.text.toString()
-            userName =binding.etUserName.text.toString()
-            email =binding.etEmail.text.toString()
-            password =binding.etFullName.text.toString()
-            val checkedGenderRadioButton=binding.radioGroup.checkedRadioButtonId
-            gender =findViewById<RadioButton>(checkedGenderRadioButton).text.toString()
 
-            editor.apply {
-                putString("Full Name",name)
-                putString("User Name",userName)
-                putString("Email",email)
-                putString("PassWord",password)
-                putString("Gender",gender)
-                apply()
+            if (binding.etFullName.text.toString().length < 5)
+                binding.etFullName.error="Your name cannot be less than 5 characters"
+
+            else if (binding.etUserName.text.toString().length < 4)
+                binding.etUserName.error="Your username cannot be less than 4 characters"
+
+
+            if (binding.etRetypePassword.text.toString()!=binding.etPassword.text.toString())
+                binding.etRetypePassword.error=" password and retyped passwords don't match!"
+            else {
+                name = binding.etFullName.text.toString()
+                userName = binding.etUserName.text.toString()
+                email = binding.etEmail.text.toString()
+                password = binding.etPassword.text.toString()
+                val checkedGenderRadioButton = binding.radioGroup.checkedRadioButtonId
+                gender = findViewById<RadioButton>(checkedGenderRadioButton).text.toString()
+
+                editor.apply {
+                    putString("Full Name", name)
+                    putString("User Name", userName)
+                    putString("Email", email)
+                    putString("PassWord", password)
+                    putString("Gender", gender)
+                    apply()
+                }
+                Toast.makeText(this, "Registration was successful.", Toast.LENGTH_SHORT).show()
             }
         }
+        binding.btnShowInfo.setOnClickListener {
 
-//        binding.btnShowInfo.setOnClickListener {
-//            binding.tvShowInfo.text="Full name: ${sharedPreferences.getString(name,name)}\n" +
-//                    "User name:${sharedPreferences.getString(userName, userName)}\n" +
-//                    "Email:${sharedPreferences.getString(email, email)}\n" +
-//                    "Password: ${sharedPreferences.getString(password, password)}\n" +
-//                    "Full name: ${sharedPreferences.getString(gender, gender)}\n"
-//
-//        }
+            binding.tvFullNameResult.text=sharedPreferences.getString(name,name)
+            binding.tvUserNameResult.text=sharedPreferences.getString(userName,userName)
+            binding.tvEmailResult.text=sharedPreferences.getString(email,email)
+            binding.tvPasswordResult.text=sharedPreferences.getString(password,password)
+            binding.tvGenderResult.text=sharedPreferences.getString(gender,gender)
 
+            binding.tvFullNameResult.visibility = View.VISIBLE
+            binding.tvUserNameResult.visibility = View.VISIBLE
+            binding.tvEmailResult.visibility = View.VISIBLE
+            binding.tvPasswordResult.visibility = View.VISIBLE
+            binding.tvGenderResult.visibility = View.VISIBLE
+            binding.btnHideInfo.visibility=View.VISIBLE
+        }
+
+        binding.btnHideInfo.setOnClickListener {
+            binding.tvFullNameResult.visibility = View.GONE
+            binding.tvUserNameResult.visibility = View.GONE
+            binding.tvEmailResult.visibility = View.GONE
+            binding.tvPasswordResult.visibility = View.GONE
+            binding.tvGenderResult.visibility = View.GONE
+            binding.btnHideInfo.visibility=View.GONE
+
+        }
     }
 }
